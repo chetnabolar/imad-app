be92c43dab46c5463e0270e5209d886098f30c4e
chetnabolar <30684893+chetnabolar@users.noreply.github.com>
2017-08-04 23:28:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send('file one server here');
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
